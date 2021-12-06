8dc1a20e2aa9b592ec75ce0413e29ad9b6c78c5f
igarg2001 <igarg2001@gmail.com>
2021-12-06 16:32:16 +0530

    gc mem exception fixed

--- src/main/java/SparkAppMain.java
+++ src/main/java/SparkAppMain.java
@@ -1,5 +1,3 @@
-
-
 import org.apache.commons.math3.stat.correlation.Covariance;
 import org.apache.spark.api.java.function.FlatMapFunction;
 import org.apache.spark.sql.Dataset;
@@ -7,7 +5,6 @@ import org.apache.spark.sql.Encoders;
 import org.apache.spark.sql.SparkSession;
 import scala.Tuple2;
 import java.io.*;
-import java.lang.reflect.Array;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
@@ -17,23 +14,29 @@ import java.util.stream.Stream;
 import org.apache.commons.math3.stat.regression.OLSMultipleLinearRegression;
 import org.apache.commons.math3.random.MersenneTwister;
 import org.apache.commons.math3.distribution.MultivariateNormalDistribution;
-import spire.math.prime.SieveUtil;
+
+import javax.xml.crypto.Data;
 
 public class SparkAppMain {
     public static List<Tuple2<LocalDate, Double>> readGoogleHistory(File file) throws FileNotFoundException, IOException {
-        System.out.println("-----------------------------------");
+       // System.out.println("-----------------------------------");
+     //   System.out.println(file.getName());
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d-MMM-yy");
         BufferedReader br = new BufferedReader(new FileReader(file));
         List<String> lines = new ArrayList<String>();
         String st;
+        int i=0;
         while((st = br.readLine())!=null) {
-            System.out.println(st);
+           // System.out.println(st);
+            ++i;
+            if(i==1) continue;
             lines.add(st);
         }
         System.out.println("-----------------------------------");
         List<Tuple2<LocalDate, Double>> ret = new ArrayList<Tuple2<LocalDate, Double>>();
         for(String line : lines) {
             String[] cols = line.split(",");
+           // System.out.println(cols[0]);
             LocalDate date =  LocalDate.parse(cols[0], formatter);
             double value = Double.parseDouble(cols[4]);
             ret.add(new Tuple2<>(date, value));
@@ -52,7 +55,6 @@ public class SparkAppMain {
         }
         return history;
     }
-
     public static List<Tuple2<LocalDate, Double>> fillInHistory(List<Tuple2<LocalDate, Double>> history, LocalDate start, LocalDate end) {
         List<Tuple2<LocalDate, Double>> cur = new ArrayList<>(history);
         List<Tuple2<LocalDate, Double>> filled = new  ArrayList<Tuple2<LocalDate, Double>>();
@@ -69,8 +71,8 @@ public class SparkAppMain {
         }
         return filled;
     }
-
     public static List<Double> twoWeekReturns(List<Tuple2<LocalDate, Double>> history) {
+       // System.out.println(history.size());
         List<Double> ret = new ArrayList<Double>();
         int i=0;
         while(i<= history.size()-1 && (i+9)<=history.size()-1) {
@@ -79,11 +81,10 @@ public class SparkAppMain {
             double prev = subList.get(0)._2;
             double elem = (next-prev)/prev;
             ret.add(elem);
+            i+=9;
         }
         return ret;
     }
-
-
     public static Tuple2<List<List<Double>>, List<List<Double>>> readStocksAndFactors() throws IOException {
         LocalDate start = LocalDate.of(2009, 10, 23);
         LocalDate end = LocalDate.of(2014, 10, 23);
@@ -107,8 +108,8 @@ public class SparkAppMain {
         }
         List<List<Tuple2<LocalDate, Double>>> stocks = allStocks.stream().map(s -> trimToRegion(s, start, end)).map(s -> fillInHistory(s, start, end)).collect(Collectors.toList());
         List<List<Tuple2<LocalDate, Double>>> factors = rawFactors.stream().map(f -> trimToRegion(f, start, end)).map(f -> fillInHistory(f, start, end)).collect(Collectors.toList());
-        List<List<Double>> stocksReturns = stocks.stream().map(s -> twoWeekReturns(s)).collect(Collectors.toList());
-        List<List<Double>> factorsReturns = factors.stream().map(f ->twoWeekReturns(f)).collect(Collectors.toList());
+        List<List<Double>> stocksReturns = stocks.stream().map(SparkAppMain::twoWeekReturns).collect(Collectors.toList());
+        List<List<Double>> factorsReturns = factors.stream().map(SparkAppMain::twoWeekReturns).collect(Collectors.toList());
         return new Tuple2<List<List<Double>>, List<List<Double>>>(stocksReturns, factorsReturns);
     }
 
@@ -121,13 +122,11 @@ public class SparkAppMain {
         }
         return mat;
     }
-
     public static List<Double> featurize(List<Double> factorReturns) {
        List<Double> squaredReturns = factorReturns.stream().map(x -> Math.signum(x)*x*x).collect(Collectors.toList());
        List<Double> squaredRootedReturns = factorReturns.stream().map(x -> Math.signum(x) * Math.sqrt(Math.abs(x))).collect(Collectors.toList());
        return Stream.of(factorReturns, squaredReturns, squaredRootedReturns).flatMap(Collection::stream).collect(Collectors.toList());
     }
-
     public static double[][] doubleNestedListto2DArray(List<List<Double>> list) {
         double[][] array = new double[list.size()][];
         for(int i=0; i<array.length; ++i) {
@@ -140,18 +139,15 @@ public class SparkAppMain {
         }
         return array;
     }
-
     public static OLSMultipleLinearRegression linearModel(List<Double> instrument, List<List<Double>> factorMatrix) {
         OLSMultipleLinearRegression regression = new OLSMultipleLinearRegression();
         double[] instrumentArr = instrument.stream().mapToDouble(Double::doubleValue).toArray();
         regression.newSampleData(instrumentArr, doubleNestedListto2DArray(factorMatrix));
         return regression;
     }
-
     public static List<List<Double>> computeFactorWeights(List<List<Double>> stockReturns, List<List<Double>> factorFeatures) {
         return stockReturns.stream().map(s -> linearModel(s, factorFeatures)).map(s -> DoubleStream.of(s.estimateRegressionParameters()).boxed().collect(Collectors.toList())).collect(Collectors.toList());
     }
-
     public static List<Double> trialReturns(Long seed, int numTrials, List<List<Double>> instruments, List<Double> factorMeans, double[][] factorCovariances) {
         MersenneTwister rand = new MersenneTwister(seed);
         MultivariateNormalDistribution multivariateNormalDistribution = new MultivariateNormalDistribution(rand, factorMeans.stream().mapToDouble(d -> d).toArray(), factorCovariances);
@@ -163,7 +159,6 @@ public class SparkAppMain {
         }
         return tReturns;
     }
-
     public static double trialReturn(List<Double> trial, List<List<Double>> instruments) {
         double totalReturn = 0.0;
         for(List<Double> instrument : instruments) {
@@ -199,8 +194,22 @@ public class SparkAppMain {
       Dataset<Long> seedsDS = spark.createDataset(seeds, Encoders.LONG()).repartition(parallelism);
       return seedsDS.flatMap((FlatMapFunction<Long, Double>) seed -> trialReturns(seed, numTrials/parallelism, factorWeights, factorMeans, factorCov).iterator(), Encoders.DOUBLE());
     }
+
+    public static double fivePercentVaR(Dataset<Double> trials) {
+        double[] quantiles = trials.stat().approxQuantile("value", new double[] {0.05}, 0.0);
+        return quantiles[0];
+    }
+//    public static double fivePercentCVar(Dataset<Double> trials) {
+//        Dataset<Double> topLosses = trials.orderBy("value").limit(Math.max((int)(trials.count())/20, 1));
+//
+//    }
     public static void main(String[] args) throws IOException {
         SparkSession sparkSession = SparkSession.builder().getOrCreate();
-        Tuple2<List<List<Double>>, List<List<Double>>> stocksFactorsRetuens = readStocksAndFactors();
+        Tuple2<List<List<Double>>, List<List<Double>>> stocksFactorsReturns = readStocksAndFactors();
+        int numTrials = 10000000;
+        int parallelism = 1000;
+        long baseSeed = 1001L;
+        Dataset<Double> trials = computeTrialReturns(stocksFactorsReturns._1, stocksFactorsReturns._2, baseSeed, numTrials, parallelism, sparkSession);
+        trials.show();
     }
 }
